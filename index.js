fb70460205fb3b3d631b5385baabf85c9d37ccb2
Leslie Borst <leslieaborst@Is-MacBook-Pro.localdomain>
2019-01-15 10:26:19 -0500

    A shopping list app with Vuejs

---
+++ index.js
@@ -0,0 +1,44 @@
+"use strict"
+
+new Vue ({
+    el: '#shopping-list',
+    data: {
+        state: 'default',
+        header: 'shopping list app',
+        newItem: '',
+        items: [
+            {
+                label: '10 party hats',
+                purchased: false,
+            },
+            {
+                label: '2 board games',
+                purchased: false,
+            },
+            {
+                label: '20 cups',
+                purchased: false,
+            },
+        ]
+    },
+    methods: {
+        saveItem: function() {
+            this.items.push({
+                label: this.newItem,
+                purchased: false,
+            },);
+            this.newItem = '';
+        },
+        changeState: function(newState) {
+            this.state = newState;
+            this.newItem = '';
+        },
+        togglePurchased: function(item) {
+            item.purchased = !item.purchased;
+        },
+        emptyList: function() {
+            this.items = [];
+        },
+        
+    }
+});
